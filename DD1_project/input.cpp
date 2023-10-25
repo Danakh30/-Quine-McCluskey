ae8d9d489f0afb21a251c8ca3a43a536c79d40ac
MhamadKhalil <mhamed_khalil@aucegypt.edu>
2023-10-25 21:23:43 +0300

    added a new code

--- DD1_project/input.cpp
+++ DD1_project/input.cpp
@@ -1,2 +1,37 @@
 #include<iostream>
+#include<regex>
+#include<string>
 using namespace std;
+
+void SoP(string f)
+{
+    regex pattern("[a-zA-Z]('|[a-zA-Z]|\\s*\\+\\s*?[a-zA-Z]|\\s*)*");
+    if(regex_match(f,pattern))
+    {
+        cout << "Match found!" << endl;
+    }
+    else
+    {
+        cout << "Match not found!" << endl;
+    }
+}
+
+void PoS(string f)
+{
+    regex pattern("([a-zA-Z](\\s*\\+\\s*[a-zA-Z])*)|(\\([a-zA-Z](\\s*\\+\\s*[a-zA-Z])*\\))+");
+    if(regex_match(f,pattern))
+    {
+        cout << "Match found!" << endl;
+    }
+    else
+    {
+        cout << "Match not found!" << endl;
+    }
+}
+
+int main()
+{
+    string s="a+b'' '";
+    SoP(s);
+
+}
