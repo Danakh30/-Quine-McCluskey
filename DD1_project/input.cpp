2eecf7159074c66cbcf4f076c7e1e93754b115be
MhamadKhalil <mhamed_khalil@aucegypt.edu>
2023-10-25 22:11:46 +0300

    working on getting the variables from the function

--- DD1_project/input.cpp
+++ DD1_project/input.cpp
@@ -1,14 +1,47 @@
 #include<iostream>
 #include<regex>
 #include<string>
+#include<sstream>
+#include<set>
 using namespace std;
+set<char> vars;
+
+void variables(string f, string flag)
+{
+    istringstream stream(f);
+    string temp;
+    regex pattern("[a-zA-Z]");
+    if(flag == "SoP")
+    {
+        while(getline(stream, temp, '+'))
+        {
+            for(char index : temp)
+            {
+                string charAsString(1,index);
+                if(regex_match(charAsString,pattern))
+                {
+                    vars.insert(index);
+                }
+            }
+        }
+    }
+    else if(flag == "PoS")
+    {
+        
+    }
+    for(char i : vars)
+    {
+        cout << i << endl;
+    }
+}
 
 void SoP(string f)
 {
-    regex pattern("[a-zA-Z]('|[a-zA-Z]|\\s*\\+\\s*?[a-zA-Z]|\\s*)*");
+    regex pattern("[a-zA-Z]('|[a-zA-Z]|\\s*\\+\\s*?[a-zA-Z]|\\s*)*"); //I still need to consider the cases with brakets
     if(regex_match(f,pattern))
     {
         cout << "Match found!" << endl;
+        variables(f,"SoP");
     }
     else
     {
@@ -31,7 +64,7 @@ void PoS(string f)
 
 int main()
 {
-    string s="a+b'' '";
+    string s="a";
     SoP(s);
 
 }
