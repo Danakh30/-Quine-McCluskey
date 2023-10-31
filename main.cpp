0e56c0f12139a9565cced6e43a7dbde15ecace16
OsswaCh <121960914+OsswaCh@users.noreply.github.com>
2023-11-01 00:28:13 +0200

    kmap and needed sets for the circuit
    dana did the kmaps  i just called them

--- main.cpp
+++ main.cpp
@@ -451,6 +451,75 @@ string finalBoolianFunction(pi_group ePI, pi_group nEPI , vector<pi> neededPI, s
 
 }
 
+
+
+
+pair<int, int> RowColumn(int literals) {
+    switch (literals) {
+    case (2):
+        return { 2,2 };
+
+    case (3):
+        return { 2,4 };
+    case (4):
+        return { 4,4 };
+    default:
+        return { 0,0 };
+    }
+}
+
+
+vector<vector<char>> CreateKmap(set<int> minterm, int Noliterals) {
+    auto dimensions = RowColumn(Noliterals);
+    const int val[] = { 0,1,3,2 };
+    auto row = dimensions.first;
+    auto col = dimensions.second;
+    vector<vector<char>> kmap(row, vector<char>(col));
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            int value = (val[i] << 2) + val[j];
+            if (find(minterm.begin(), minterm.end(), value) != minterm.end()) {
+                kmap[i][j] = '1';
+            }
+            else {
+                kmap[i][j] = '0';
+            }
+        }
+    }
+    return kmap;
+}
+
+void DisplayKmap(set<int> minterm, int Noliterals) {
+    auto kmap = CreateKmap(minterm, Noliterals);
+    for (auto& row : kmap) {
+        for (auto& cell : row) {
+            cout << cell << '\t';
+        }
+        cout << '\n';
+    }
+}
+
+
+vector<string> boolianTerms(pi_group ePI, pi_group nEPI, vector<pi> neededPI, set<char> vars) {
+
+    set<string>epii = transformToBoolian(vars, ePI);
+    set<string>nepii = transformToBooleanVec(vars, neededPI);
+    
+    std::vector<string> mergedVector;
+
+    for (const auto& p : epii) {
+        mergedVector.push_back(p);
+    }
+
+    for (const auto& p : nepii) {
+        mergedVector.push_back(p);
+    }
+
+    return mergedVector;
+}
+
+
+
 int main() {
 
     set<char>vars = { 'A','B','C','D','E' };
